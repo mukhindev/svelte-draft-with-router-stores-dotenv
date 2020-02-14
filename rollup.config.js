f31e459e93a75d9c861a22a0381471574b8bce85
mukhindev <mukhin.dev@gmail.com>
2020-02-15 01:25:14 +0300

    fixed serve

--- rollup.config.js
+++ rollup.config.js
@@ -48,7 +48,7 @@ function serve () {
       if (!started) {
         started = true
 
-        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        require('child_process').spawn('npm', ['run', 'start:dev'], {
           stdio: ['ignore', 'inherit', 'inherit'],
           shell: true
         })
